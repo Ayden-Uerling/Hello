b324b36cb8de5f865a2936c511c989d827834daf
ryan-healey <ryan-healey@users.noreply.github.com>
2020-01-16 13:39:59 -0600

    Forgot the semicolons
    Whoops

--- hello.c
+++ hello.c
@@ -3,8 +3,8 @@
 int main(void) {
   printf("Ayden Uerling!\n");
   printf("Electrical Engineering!!\n");
-  printf("Ryan Healey\n")
-  printf("Electrical Engineering\n")
+  printf("Ryan Healey\n");
+  printf("Electrical Engineering\n");
   return 0;
 
 }
